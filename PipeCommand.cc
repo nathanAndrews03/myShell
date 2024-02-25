5ccb2839e8c0d2305f73c9b6129fea64040b6228
Nathan Robert Andrews <andre149@data.cs.purdue.edu>
2024-02-25 00:16:47 -0500

    Commit

--- PipeCommand.cc
+++ PipeCommand.cc
@@ -131,6 +131,54 @@ void PipeCommand::execute() {
     }
     waitpid(ret, NULL, 0);
 
+
+    	//save in/out
+	int tmpin=dup(0);
+	int tmpout=dup(1);
+	//set the initial input
+	int fdin;
+	if (infile) {
+		fdin = open(infile,......);
+	} else {
+		// Use default input
+		fdin=dup(tmpin);
+	}
+	int ret;
+	int fdout;
+	for(i=0; i<numsimplecommands; i++) {
+		//redirect input
+		dup2(fdin, 0);
+		close(fdin);
+		//setup output
+		if (i == numsimplecommands-1){
+		// Last simple command
+			if(outfile){
+				fdout=open(outfile,......);
+			} else {
+				// Use default output
+				fdout=dup(tmpout);
+			}
+		} else {
+			// Not last
+			//simple command
+			//create pipe
+			int fdpipe[2];
+			pipe(fdpipe);
+			fdout=fdpipe[1];
+			fdin=fdpipe[0];
+		}// if/else
+		// Redirect output
+		dup2(fdout,1);
+		close(fdout);
+		// Create child process
+		ret=fork();
+		if(ret==0) {
+			execvp(scmd[i].args[0],
+			scmd[i].args);
+			perror(“execvp”);
+			exit(1);
+		}
+	} // for
     // Clear to prepare for next command
     clear();
 
