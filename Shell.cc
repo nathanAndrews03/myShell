7110c1915a660d0a2f416ea0b27a3f4d2d1f3419
Nathan Robert Andrews <andre149@data.cs.purdue.edu>
2024-03-06 16:33:22 -0500

    Commit

--- Shell.cc
+++ Shell.cc
@@ -96,7 +96,7 @@ int main(int argc, char **argv) {
   sa_z.sa_handler = zInterrupt;
   sa_z.sa_flags = SA_RESTART;
   tmp = sigaction(SIGCHLD, &sa_z, NULL);
-  if (tmp = -1) {
+  if (tmp == -1) {
 	perror("sigaction");
 	exit(1);
   }
