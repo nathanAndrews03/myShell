739c91ce0cb42aae36d288c228563873979a94a3
Nathan Robert Andrews <andre149@data.cs.purdue.edu>
2024-03-06 16:49:50 -0500

    Commit

--- Shell.cc
+++ Shell.cc
@@ -67,7 +67,7 @@ void cInterrupt(int sig) {
 void zInterrupt(int sig) {
 	pid_t pid;
 	while ((pid = waitpid(-1, NULL, WNOHANG)) > 0); 
-	printf(pid + " exited\n");
+	printf("%d exited\n", pid);
 	Shell::TheShell->prompt();
 }
  
